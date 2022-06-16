a08552ec7614af14f4aa4bc7a7b8b869a96510a7
Thaddeus <thaddeusleezx@gmail.com>
2022-06-16 21:51:24 +0800

    first working version with local db

--- bot.py
+++ bot.py
@@ -1,31 +1,227 @@
+# from telegram import Update
+# import logging
+# from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+# import os
+
+# PORT = int(os.environ.get('PORT', '5000'))
+
+# TOKEN = '5440405771:AAFEySa6ebHb0s64V_tQs8Ip2LntwnQtgpA'
+
+# logging.basicConfig(
+#     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+#     level=logging.INFO
+# )
+
+# async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+#     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+# if __name__ == '__main__':
+#     application = ApplicationBuilder().token(TOKEN).build()
+    
+#     start_handler = CommandHandler('start', start)
+#     application.add_handler(start_handler)
+#     application.run_webhook(
+# 		listen="0.0.0.0",
+# 		port=int(PORT),
+#         url_path=TOKEN,
+#         webhook_url='https://telegram-claims-bot.herokuapp.com/' + TOKEN
+#     )
+
 import logging
-from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
-import os
-
-PORT = int(os.environ.get('PORT', '5000'))
-
-TOKEN = '5440405771:AAFEySa6ebHb0s64V_tQs8Ip2LntwnQtgpA'
+import database
+from telegram import Update, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters, ConversationHandler
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+CHOICE, VIEW, ADD, REMOVE, CLEAR, DONE = range(6)
+
+def format_input_add(user_data):
+	facts = [f"{key} - {value}" for key, value in user_data.items()]
+	return "\n".join(facts).join(["\n", "\n"])
+
+def format_result_view(results):
+	index = 1
+	output = ["No. Date  Desc   Amt"]
+	total = 0
+	for row in results:
+		date = row[1].__format__("%d/%m")
+		output.append(f"{str(index)}. {date}  {row[3]}   ${row[4]}")
+		total += row[4]
+		index += 1
+	output.append(f"Total: ${total}")
+	return "\n".join(output)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message with a button that opens a the web app."""
+    await update.message.reply_text(
+        f"Hi {update.message.chat.username}! What would you like to do?",
+        reply_markup=ReplyKeyboardMarkup(
+			keyboard=[
+				[KeyboardButton(text="View claims"),
+				KeyboardButton(text="Add claim")],
+				[KeyboardButton(text="Remove claim"),
+				KeyboardButton(text="Clear all claims")]
+			],
+			one_time_keyboard=True
+		)
+    )
+    return CHOICE
+
+async def view(update: Update, context: ContextTypes.DEFAULT_TYPE):
+	user_claims = database.get_claims(update.message.chat.username)
+	if isinstance(user_claims, str):
+		await update.message.reply_text(
+		f"{user_claims}"
+    )
+	else:
+		format_claims = format_result_view(user_claims)
+		await update.message.reply_text(
+			f"{format_claims}"
+		)
+		user_claims.clear()
+	return ConversationHandler.END
+
+
+async def add_date(update: Update, context: ContextTypes.DEFAULT_TYPE):
+	await update.message.reply_text(
+        "Please enter the day of the purchase (DD/MM)"
+    )
+	return ADD
+	
+async def add_description(update: Update, context: ContextTypes.DEFAULT_TYPE):
+	text = update.message.text
+	context.user_data['Date'] = text
+	await update.message.reply_text(
+        "Please enter a short description of the purchase"
+    )
+	return ADD
+
+async def add_amount(update: Update, context: ContextTypes.DEFAULT_TYPE):
+	text = update.message.text
+	context.user_data["Desc"] = text
+	await update.message.reply_text(
+        "Please enter the amount spent (without $)"
+    )
+	return ADD
+
+async def add_confirm(update: Update, context: ContextTypes.DEFAULT_TYPE):
+	text = update.message.text
+	context.user_data["Amount"] = text
+	user_input = context.user_data
+	await update.message.reply_text(
+		f"I got this data from you: {format_input_add(user_input)}Would you like to confirm or restart?",
+		reply_markup=ReplyKeyboardMarkup(
+			keyboard=[
+				[KeyboardButton(text="Confirm"),
+				KeyboardButton(text="Restart")],
+			],
+			one_time_keyboard=True
+		)
+    )
+	return ADD
+
+async def add_complete(update: Update, context: ContextTypes.DEFAULT_TYPE):
+	user_input = context.user_data
+	success_message = database.add_claim(update.message.chat.username, user_input['Date'], user_input['Desc'], user_input['Amount'])
+	user_claims = database.get_claims(update.message.chat.username)
+	if isinstance(user_claims, str):
+		await update.message.reply_text(
+			"Unable to add or view current claims. Please contact @ebilsanta for help!"
+		)
+	else:
+		format_claims = format_result_view(user_claims)
+		await update.message.reply_text(
+			f"{success_message}\nThese are your current claims:\n{format_claims}"
+			, reply_markup=ReplyKeyboardRemove()
+		)
+	user_input.clear()
+	return ConversationHandler.END
+
+async def remove(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user_claims = database.get_claims(update.message.chat.username)
+    format_claims = format_result_view(user_claims)
+    await update.message.reply_text(
+        f"These are the existing claims, which number would you like to remove?\n{format_claims}"
+    )
+    return REMOVE
+
+async def remove_complete(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+	index = update.message.text
+	success_message = database.delete_claim(update.message.chat.username, index)
+	user_claims = database.get_claims(update.message.chat.username)
+	format_claims = format_result_view(user_claims)
+	await update.message.reply_text(
+        f"{success_message} These are the existing claims.\n{format_claims}"
+    )
+	return ConversationHandler.END
+
+async def clear(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user_claims = database.get_claims(update.message.chat.username)
+    format_claims = format_result_view(user_claims)
+    await update.message.reply_text(
+        f"These are the existing claims, are you sure you want to clear everything?\n{format_claims}",
+		reply_markup=ReplyKeyboardMarkup(
+			keyboard=[
+				[KeyboardButton(text="Yes"),
+				KeyboardButton(text="Nope, cancel")],
+			],
+			one_time_keyboard=True
+		)
+    )
+    return CLEAR
+
+async def clear_complete(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+	database.clear_claims(update.message.chat.username)
+	await update.message.reply_text(
+        f"All claims cleared successfully."
+    )
+	return ConversationHandler.END
+
+async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token(TOKEN).build()
-    
-    start_handler = CommandHandler('start', start)
-    application.add_handler(start_handler)
-    application.run_webhook(
-		listen="0.0.0.0",
-		port=int(PORT),
-        url_path=TOKEN,
-        webhook_url='https://telegram-claims-bot.herokuapp.com/' + TOKEN
-    )
+	application = ApplicationBuilder().token('5440405771:AAFEySa6ebHb0s64V_tQs8Ip2LntwnQtgpA').build()
 
+	start_handler = ConversationHandler(
+		entry_points=[CommandHandler("start", start),
+						MessageHandler(filters.Regex("Add claim"), add_date),
+						MessageHandler(filters.Regex("View claims"), view),
+						MessageHandler(filters.Regex("Remove claim"), remove),
+						MessageHandler(filters.Regex("Clear all claims"), clear)
+					],
+		states={
+			CHOICE: [
+				MessageHandler(filters.Regex("Add claim"), add_date),
+				MessageHandler(filters.Regex("View claims"), view),
+				MessageHandler(filters.Regex("Remove claim"), remove),
+				MessageHandler(filters.Regex("Clear all claims"), clear)
+
+			],
+			ADD: [	
+				MessageHandler(filters.Regex(r"/"), add_description),
+				MessageHandler(filters.Regex("Restart"), add_date),
+				MessageHandler(filters.Regex("Confirm"), add_complete),
+				MessageHandler(filters.Regex("[^0-9/.]"), add_amount),
+				MessageHandler(filters.Regex("[^a-zA-Z]") & filters.Regex("^((?!/).)*$"), add_confirm)
+			],
+			REMOVE: [
+				MessageHandler(filters.Regex("[0-9]"), remove_complete)
+			],
+			CLEAR: [
+				MessageHandler(filters.Regex("Yes"), clear_complete)
+			]
+		},
+		fallbacks=[MessageHandler(filters.Regex("Done"), add_complete)]
+	)
+
+	application.add_handler(start_handler)
+
+	application.run_polling()
 
 
---
+++ database.py
@@ -0,0 +1,96 @@
+import mysql.connector
+from mysql.connector import Error
+
+def add_claim(username, date, description, amount):
+	try:
+		month = date[3:]
+		day = date[:2]
+		f_date = f"2022-{month}-{day}"
+		connection = mysql.connector.connect(host='localhost',
+											database='claims',
+											user='root',
+											password='root')
+		cursor = connection.cursor()
+		mySql_insert_query = """INSERT INTO claims (username, date, description, amount) 
+							VALUES (%s, %s, %s, %s)"""
+		record = (username, f_date, description, str(amount))
+		cursor.execute(mySql_insert_query, record)
+		connection.commit()
+		connection.close()
+		cursor.close()
+		print("MySQL connection is closed")
+		return "Claim has been added successfully"
+
+	except mysql.connector.Error as error:
+		return "Error adding claim, please check formatting!"
+
+def get_claims(username):
+	try:
+		connection = mysql.connector.connect(host='localhost',
+											database='claims',
+											user='root',
+											password='root')
+
+		sql_select_Query = "select * from claims where username = %s"
+
+		cursor = connection.cursor()
+		cursor.execute(sql_select_Query, (username,))
+		records = cursor.fetchall()
+		print("Total number of rows in table: ", cursor.rowcount)
+
+		print("\nPrinting each row")
+		for row in records:
+			print("username = ", row[0])
+			print("date = ", row[1])
+			print("claimId = ", row[2])
+			print("description = ", row[3])
+			print("amount = ", row[4], "\n")
+		connection.close()
+		cursor.close()
+		print("MySQL connection is closed")
+		return records
+
+	except mysql.connector.Error as e:
+		return "Error getting claims, please contact @ebilsanta for help"
+
+def delete_claim(username, index):
+	try:
+		connection = mysql.connector.connect(host='localhost',
+											database='claims',
+											user='root',
+											password='root')
+		cursor = connection.cursor()
+		sql_Delete_query = """DELETE FROM claims WHERE claimId = (
+								SELECT max(claimId) FROM (
+									SELECT claimId FROM claims WHERE username = %s LIMIT %s
+									) as temp
+								)"""
+
+		cursor.execute(sql_Delete_query, (username, int(index),))
+		connection.commit()
+		connection.close()
+		cursor.close()
+		print("MySQL connection is closed")
+		return "Claim removed successfully!"
+
+	except mysql.connector.Error as error:
+		return "Failed to delete claim, please check submitted number or contact @ebilsanta for help"
+
+def clear_claims(username):
+	try:
+		connection = mysql.connector.connect(host='localhost',
+											database='claims',
+											user='root',
+											password='root')
+		cursor = connection.cursor()
+		sql_Delete_query = "DELETE FROM claims WHERE username = %s"
+		# row to delete
+		cursor.execute(sql_Delete_query, (username,))
+		connection.commit()
+		print("Records Deleted successfully ")
+		connection.close()
+		cursor.close()
+		print("MySQL connection is closed")
+
+	except mysql.connector.Error as error:
+		print("Failed to Delete record from table: {}".format(error))
